9d42daa402101fd9f820ada690f873209126ba66
gamaliel <gamaliel@facturatech.co>
2022-11-22 13:06:36 -0600

    configuracion principal middlewares y error principal

---
+++ index.js
@@ -0,0 +1,13 @@
+const container = require("./src/startup/container");
+const server = container.resolve("app");
+const { MONGO_URI } = container.resolve("config");
+
+const mongoose = require("mongoose");
+//console.log(mongoose.set());
+//mongoose.set("useCreateIndex", true);
+
+mongoose
+    .connect(MONGO_URI)
+    .then(() => server.start())
+    .catch(console.log);
+
